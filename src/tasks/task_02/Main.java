bea96e0a4763e3068c13ca5c8f1c03b850eb4a74
Yaroslav82 <yargay230607@gmail.com>
2023-07-05 13:40:57 +0300

    Fix task_02 solution

--- src/tasks/task_02/Main.java
+++ src/tasks/task_02/Main.java
@@ -1,32 +1,44 @@
 package tasks.task_02;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     private final static int[] PRICES = new int[] {25, 12, 43, 20, 42};
 
+    private final static Scanner scanner = new Scanner(System.in);
+
     private final static String DEFAULT_ENTER = "Enter index: ";
     private final static String DEFAULT_OUTPUT = "The price is ";
+    private final static String INVALID_INPUT = "Invalid value!";
+    private final static String INVALID_INDEX = "Index out of bounds!";
 
     public static void main(String[] args) {
         getOutput(findPrice(getIndex()));
     }
 
     private static int getIndex() {
-        try (Scanner scanner = new Scanner(System.in)) {
+        try {
             System.out.print(DEFAULT_ENTER);
-            return scanner.nextInt();
-        } catch (InputMismatchException e) {
-            throw new RuntimeException(e);
+            int index = Integer.parseInt(scanner.nextLine());
+
+            if (validateIndex(index)) {
+                return index;
+            } else {
+                System.out.println(INVALID_INDEX);
+                return getIndex();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(INVALID_INPUT);
+            return getIndex();
         }
     }
 
+    private static boolean validateIndex(int index) {
+        return index >= 0 && index < PRICES.length;
+    }
+
     private static int findPrice(int index) {
-        if (index < 0 || index >= PRICES.length) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
         return PRICES[index];
     }
 
