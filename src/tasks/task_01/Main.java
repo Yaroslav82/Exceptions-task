1dc1e1cfe60b4c35f76bf3efa72fffe03d0f9d95
Yaroslav82 <yargay230607@gmail.com>
2023-07-05 13:26:39 +0300

    Fix task_01 solution

--- src/tasks/task_01/Main.java
+++ src/tasks/task_01/Main.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
+    private final static Scanner scanner = new Scanner(System.in);
+
     private final static String DEFAULT_ENTER = "Enter a number: ";
     private final static String DEFAULT_OUTPUT = "The number is ";
+    private final static String INVALID_INPUT = "Invalid value!";
 
     public static void main(String[] args) {
         getOutput(getData());
     }
 
     private static double getData() {
-        try (Scanner scanner = new Scanner(System.in)) {
+        try {
             System.out.print(DEFAULT_ENTER);
             // Лучше использовать scanner.nextDouble(), однако в таком случае получить NumberFormatException невозможно
             return Double.parseDouble(scanner.nextLine());
         } catch (NumberFormatException e) {
-            throw new RuntimeException(e);
+            System.out.println(INVALID_INPUT);
+            return getData();
         }
     }
 
